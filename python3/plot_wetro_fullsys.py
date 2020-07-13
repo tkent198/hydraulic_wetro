890ac2816295af26dbedb3e9700f03b092329d85
tkent198 <tkent198@gmail.com>
2020-07-13 17:46:48 +0100

    plot gridspec

--- python3/plot_wetro_fullsys.py
+++ python3/plot_wetro_fullsys.py
@@ -14,6 +14,7 @@ import importlib.util
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import pdb # Python DeBugging
+import matplotlib.gridspec as gs
 
 
 ##################################################################
@@ -144,13 +145,13 @@ Qmin = 0
 hmmax = 0.12
 hmmin = 0
 
-fp = index_fp[50]
+fp = index_fp[60]
 ct = index_city[5]
 
 # plt.ion() ## Note this correction
 
-
-while T < tmax:
+'''
+while tn <= tmax:
 
     h =  h_array[:,:,T][0]
     A =  U_array[0,:,T]
@@ -248,14 +249,14 @@ while T < tmax:
     X,Y,Xc,Yc,__ = plot_xsec_hAs(U_array[0,fp+1,T],s[fp],config)
     axes[2,2].plot(Xc,Yc,'k', linewidth=2.0)
     axes[2,2].fill(X,Y,'b',alpha=0.2)
-    axes[2,2].text(Xc[-1],0.5*config.hr,'$t=%.3g$' %tmeasure, fontsize=12, horizontalalignment='right')
+    axes[2,2].text(Xc[-1],0.5*config.hr,'$t=%.3g$' %tn, fontsize=12, horizontalalignment='right')
     axes[2,2].text(Xc[-1],0.25*config.hr,'$s=%.3g$' %s[fp],fontsize=12, horizontalalignment='right')
 
     ## h cross-section: city
     X,Y,Xc,Yc,__ = plot_xsec_hAs(U_array[0,ct+1,T],s[ct],config)
     axes[2,3].plot(Xc,Yc,'k', linewidth=2.0)
     axes[2,3].fill(X,Y,'b',alpha=0.2)
-    axes[2,3].text(Xc[-1],0.5*config.hr,'$t=%.3g$' %tmeasure, fontsize=12, horizontalalignment='right')
+    axes[2,3].text(Xc[-1],0.5*config.hr,'$t=%.3g$' %tn, fontsize=12, horizontalalignment='right')
     axes[2,3].text(Xc[-1],0.25*config.hr,'$s=%.3g$' %s[ct],fontsize=12, horizontalalignment='right')
 
     plt.tight_layout(pad=0.2, w_pad=0.01, h_pad=0.01)
@@ -263,4 +264,136 @@ while T < tmax:
     plt.show()
     plt.pause(0.1)
 
+    tn += dtmeasure
+    T += 1
+'''
+
+
+
+while tn <= tmax:
+
+    h =  h_array[:,:,T][0]
+    A =  U_array[0,:,T]
+    Au = U_array[1,:,T]
+
+    # plt.ion() ## Note this correction
+
+    # fig, axes = plt.subplots(3, 4, figsize=(13,7))#, constrained_layout=True)
+
+    fig = plt.figure(figsize=(13, 6.5))
+    G = gs.GridSpec(3, 4)
+    ax_rt = plt.subplot(G[0,0]) # Rain factor against t
+    ax_rhist = plt.subplot(G[0,1]) # Rain hist
+    ax_Aus = plt.subplot(G[0,2:]) # River: Au(s,t)
+    ax_hm = plt.subplot(G[1,0]) # Moor: hm(y,t)
+    ax_cr = plt.subplot(G[1,1]) # Canals and res.: h(t)
+    ax_hs = plt.subplot(G[1, 2:]) # River: h(s,t)
+    ax_Qh = plt.subplot(G[2,0]) # Rating curve: Q = Q(h)
+    ax_hct = plt.subplot(G[2,1]) # h(s,t) in city with flood theshold
+    ax_hxfp = plt.subplot(G[2,2]) # X-sec: fp
+    ax_hxct = plt.subplot(G[2,3]) # X-sec: ct
+
+    ## Rainfall: times series
+    ax_rt.plot(timevec[:T],Rm[:T], marker = '$M$', linestyle = 'None')
+    ax_rt.plot(timevec[:T],Rr[:T], marker = '$R$', linestyle = 'None')
+    ax_rt.plot(timevec[:T],Rm[:T]+Rr[:T], marker = '$&$', linestyle = 'None')
+    ax_rt.set_ylim(-0.5, 20)
+    ax_rt.set_yticks(rainfac)
+    ax_rt.set_yticklabels(rainfac)
+    # axes[0,2].set_xlim(0, tmeasure)
+
+    ## Rainfall: histogram
+    hist, bin_edges = np.histogram(Rm[:T]+Rr[:T], bins = np.arange(0,20,1), density=True)
+    # print('hist', hist)
+    # print('bins', bin_edges)
+    bin_edges = np.round(bin_edges,0)
+    ax_rhist.bar(bin_edges[:-1], hist, width = 1, color='#0504aa',alpha=0.7)
+    # plt.xlim(min(bin_edges), max(bin_edges))
+    ax_rhist.plot(rainfac,rainpdf,'ko')
+    ax_rhist.set_xlabel('Rainfall amount')
+    ax_rhist.set_ylabel('Density')
+    # axes[1,2].title('Histogram of rainfall amounts')
+    ax_rhist.set_xlim(-1, 19)
+    ax_rhist.set_xticks(rainfac)
+    ax_rhist.set_xticklabels(rainfac)
+
+    ## Moor
+    ax_hm.plot(yy,hm_array[:,T])
+    ax_hm.set_xlim([0,Ly])
+    ax_hm.set_ylim([hmmin,hmmax])
+
+    ## Q-h relationship in city (a la rating curve)
+    # if (hct[1:]>hct[:-1]):
+    #     axes[2,0].plot(hct,Qct,'2k')
+    # else:
+    #     axes[2,0].plot(hct,Qct,'1b')
+    hct = h_array[0,ct+1,:T]
+    Qct = U_array[1,ct+1,:T]
+    ax_Qh.plot(hct[np.where(hct[1:]>hct[:-1])],Qct[np.where(hct[1:]>hct[:-1])],'2k')
+    ax_Qh.plot(hct[np.where(hct[1:]<=hct[:-1])],Qct[np.where(hct[1:]<=hct[:-1])],'1b')
+    ax_Qh.plot([hc,hc],[Qmin+0.0001,Qmax],'r:')
+    ax_Qh.set_xlabel('h')
+    ax_Qh.set_ylabel('Q')
+    ax_Qh.set_xlim([hmin+0.01,hmax])
+    ax_Qh.set_ylim([Qmin+0.0001,Qmax])
+    ax_Qh.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+    ax_Qh.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+
+    ## Canals and res: time series
+    ax_cr.plot(hcs[0,:T], marker = '$1$', linestyle = 'None')
+    ax_cr.plot(hcs[1,:T], marker = '$2$', linestyle = 'None')
+    ax_cr.plot(hcs[2,:T], marker = '$3$', linestyle = 'None')
+    ax_cr.plot(h_res[:T]/10, marker = '$R$', linestyle = 'None')
+    ax_cr.set_ylim([0.005,0.015])
+
+    ## h(city) time series with flood threshold h_T
+    # ax_hct.plot([0,ncc-1],[hc, hc],'r:')
+    ax_hct.plot(hct)
+    ax_hct.set_ylim([0,0.04])
+
+    ## h(s,t)
+    ax_hs.plot([s[fp], s[fp]],[hmin,hmax],'r:')
+    ax_hs.plot([s[ct], s[ct]],[hmin,hmax],'r:')
+    ax_hs.fill([config.LR1, config.LR2,config.LR2,config.LR1], [0,0,config.hc,config.hc],'r',alpha=0.1,linestyle='None')
+    ax_hs.plot([s_r, s_r],[hmin,hmax],'k:')
+    ax_hs.plot([s_m, s_m],[hmin,hmax],'k:')
+    ax_hs.plot([Lc1, Lc1],[hmin,hmax],'k:')
+    ax_hs.set_ylim([hmin,hmax])
+    ax_hs.set_xlim([0,L])
+    ax_hs.set_ylabel('$h(s,t)$',fontsize=12)
+    ax_hs.set_xlabel('$s$',fontsize=12)
+    ax_hs.plot([s[:-1],s[1:]],[h_array[0,1:-1,T],h_array[0,1:-1,T]],'b', linewidth = 1.0)
+    ax_hs.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+
+    ## Au(s,t)
+    ax_Aus.set_ylim([Qmin,Qmax])
+    ax_Aus.set_xlim([0,L])
+    ax_Aus.plot([s_r, s_r],[Qmin,Qmax],'k:')
+    ax_Aus.plot([s_m, s_m],[Qmin,Qmax],'k:')
+    ax_Aus.plot([Lc1, Lc1],[Qmin,Qmax],'k:')
+    ax_Aus.set_ylabel('$Au(s,t)$',fontsize=12)
+    ax_Aus.set_xlabel('$s$',fontsize=12)
+    ax_Aus.plot([s[:-1],s[1:]],[U_array[1,1:-1,T],U_array[1,1:-1,T]],'b', linewidth = 1.0)
+    ax_Aus.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+
+    ## h cross-section: floodplain
+    X,Y,Xc,Yc,__ = plot_xsec_hAs(U_array[0,fp+1,T],s[fp],config)
+    ax_hxfp.plot(Xc,Yc,'k', linewidth=2.0)
+    ax_hxfp.fill(X,Y,'b',alpha=0.2)
+    ax_hxfp.text(Xc[-1],0.5*config.hr,'$t=%.3g$' %tn, fontsize=12, horizontalalignment='right')
+    ax_hxfp.text(Xc[-1],0.25*config.hr,'$s=%.3g$' %s[fp],fontsize=12, horizontalalignment='right')
+
+    ## h cross-section: city
+    X,Y,Xc,Yc,__ = plot_xsec_hAs(U_array[0,ct+1,T],s[ct],config)
+    ax_hxct.plot(Xc,Yc,'k', linewidth=2.0)
+    ax_hxct.fill(X,Y,'b',alpha=0.2)
+    ax_hxct.text(Xc[-1],0.5*config.hr,'$t=%.3g$' %tn, fontsize=12, horizontalalignment='right')
+    ax_hxct.text(Xc[-1],0.25*config.hr,'$s=%.3g$' %s[ct],fontsize=12, horizontalalignment='right')
+
+    plt.tight_layout(pad=0.2, w_pad=0.01, h_pad=0.01)
+
+    plt.show()
+    plt.pause(0.1)
+
+    tn += dtmeasure
     T += 1
